6da2a0b94d4e43beef5061abe0b6d6375839e5c0
fftcysstkm <t.satobm3012@gmail.com>
2024-01-01 10:20:55 +0900

    setupタグ利用

--- pages/index.vue
+++ pages/index.vue
@@ -9,58 +9,44 @@
       class="elevation-1"
       item-key="id"
     >
-    <template #item.action="{item}">
-      <v-btn color="primary" @click="showDialog(item)">表示</v-btn>
-    </template>
+      <template #item.action="{ item }">
+        <v-btn color="primary" @click="showDialog(item)">表示</v-btn>
+      </template>
     </v-data-table>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref } from 'vue';
+<script lang="ts" setup>
+import { ref } from 'vue'
 import { Post } from '@/model/Post'
 
-export default defineComponent({
-  setup() {
-    const posts = ref<Post[]>([]);
-    const headers  = ref([
-      {text: 'ID', align: 'start', value:'id'},
-      {text: 'Title', value: 'title'},
-      {text: 'Body', value: 'body'},
-      {text: 'Actions',value: 'action'}
-    ]);
+const posts = ref<Post[]>([])
+const headers = ref([
+  { text: 'ID', align: 'start', value: 'id' },
+  { text: 'Title', value: 'title' },
+  { text: 'Body', value: 'body' },
+  { text: 'Actions', value: 'action' },
+])
 
-    // データ取得
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-        const data: Post[] = await response.json();
-        posts.value = data;
-      } catch (error) {
-        console.error('データの取得に失敗しました。', error);
-      }
-    }
-
-    // データ初期化
-    const deleteData = () => {
-      posts.value = [];
-    }
-
-    //
-    const showDialog = (item: Post) => {
-      // ここにダイアログ表示ロジックを実装
-      console.log('Dialog shown for:', item);
-    };
-
-    return {
-      posts,
-      headers,
-      fetchData,
-      deleteData,
-      showDialog
-    }
+// データ取得
+const fetchData = async () => {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const data: Post[] = await response.json()
+    posts.value = data
+  } catch (error) {
+    console.error('データの取得に失敗しました。', error)
   }
+}
 
-})
+// データ初期化
+const deleteData = () => {
+  posts.value = []
+}
 
+//
+const showDialog = (item: Post) => {
+  // ここにダイアログ表示ロジックを実装
+  console.log('Dialog shown for:', item)
+}
 </script>
